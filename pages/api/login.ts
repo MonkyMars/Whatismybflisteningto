6cef386cffc6d146e64f9ab60dc9f467150ff94d
Levi Noppers <=>
2024-10-19 20:53:43 +0200

    fixing logging in

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -2,8 +2,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const scope = 'user-read-playback-state user-read-currently-playing';
-  const redirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI; // Add this to .env.local
-  const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+  const redirectUri = 'https://whatismybflisteningto.vercel.app/api/callback';
+  const clientId = '6636e187c9d64d1fb17c606d30f527ad';
 
   const spotifyAuthUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${encodeURIComponent(
     scope
@@ -11,3 +11,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   res.redirect(spotifyAuthUrl);
 }
+
+//  https://accounts.spotify.com/authorize?response_type=code&client_id=undefined&scope=user-read-playback-state%20user-read-currently-playing&redirect_uri=undefined
